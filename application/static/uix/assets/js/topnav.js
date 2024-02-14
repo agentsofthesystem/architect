eb0ca2c7e599dff27ea7d0f484a0ef127553c288
Joshua Reed <11220408+jreed1701@users.noreply.github.com>
2024-02-14 11:29:09 -0700

    Update landing page with content and nav menu.

--- application/static/uix/assets/js/topnav.js
+++ application/static/uix/assets/js/topnav.js
@@ -1,35 +1,69 @@
+function resetOpacity(menu_class) {
+    var element = document.getElementsByClassName(menu_class)[0];
+
+    if( 'opacity' in element.style){
+        element.style.setProperty('opacity', '0')
+    }
+    else{
+        element.style.opacity = 0;
+    }
+}
+
+function fadeElementIn(menu_class) {
+    var timeout = 100;
+    var element = document.getElementsByClassName(menu_class)[0];
+    var current_opacity = element.style.opacity;
+    var new_opacity = parseFloat(current_opacity);
+
+    if(current_opacity < 1) {
+       new_opacity += .075;
+       setTimeout(function(){fadeElementIn(menu_class)}, timeout);
+    }
+
+    element.style.opacity = new_opacity;
+}
 
 $(document).ready(function($){
 
     product_link = document.getElementById('product-topnav-item');
     docs_link = document.getElementById('docs-topnav-item');
 
+    main_container_class = '.main-container'
+    product_menu_class = 'product-topnav-child-menu'
+    docs_menu_class = 'docs-topnav-child-menu'
+
     if(product_link != null){
 
         document.getElementById('product-topnav-item').onmouseover = function() {
             // do something like for example change the class of a div to change its color :
-            $('.product-topnav-child-menu')[0].style = '';
-            $('.main-container')[0].style = 'display: none;';
+            $('.' + product_menu_class)[0].style = 'display: block;';
+            $(main_container_class)[0].style = 'display: none;';
+            resetOpacity(product_menu_class);
+            fadeElementIn(product_menu_class);
         };
-        
+
         document.getElementById('product-topnav-item').onmouseout = function() {
             // do something like for example change the class of a div to change its color :
-            $('.product-topnav-child-menu')[0].style = 'display: none;';
+            $('.' + product_menu_class)[0].style = 'display: none; opacity: 0;';
             $('.main-container')[0].style = '';
+            resetOpacity(product_menu_class);
         };
     }
 
     if(docs_link != null){
         document.getElementById('docs-topnav-item').onmouseover = function() {
             // do something like for example change the class of a div to change its color :
-            $('.docs-topnav-child-menu')[0].style = '';
+            $('.' + docs_menu_class)[0].style = '';
             $('.main-container')[0].style = 'display: none;';
+            resetOpacity(docs_menu_class);
+            fadeElementIn(docs_menu_class);
         };
-        
+
         document.getElementById('docs-topnav-item').onmouseout = function() {
             // do something like for example change the class of a div to change its color :
-            $('.docs-topnav-child-menu')[0].style = 'display: none;';
+            $('.' + docs_menu_class)[0].style = 'display: none;';
             $('.main-container')[0].style = '';
+            resetOpacity(docs_menu_class);
         };
     }
 
