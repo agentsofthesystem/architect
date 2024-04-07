96ca5673b019c827095b785c76f19871ebb5e1f1
Joshua Reed <11220408+jreed1701@users.noreply.github.com>
2024-04-07 12:22:27 -0600

    Fix pytest imports.

--- tests/functional/test_signup.py
+++ tests/functional/test_signup.py
@@ -1,6 +1,6 @@
 import pytest
 
-from application.workers.email import emailer
+from application.workers.email import send_email
 from application.models.user import UserSql
 from kombu.exceptions import OperationalError
 
@@ -8,7 +8,7 @@ from kombu.exceptions import OperationalError
 class TestSignup:
     @pytest.mark.skip(reason="TODO: Fix this test")
     def test_good_signup(self, client, mocker):
-        mocker.patch.object(emailer, "apply_async", return_value=None)
+        mocker.patch.object(send_email, "apply_async", return_value=None)
 
         payload = {
             "username": "TEST",
@@ -30,7 +30,7 @@ class TestSignup:
 
     @pytest.mark.skip(reason="TODO: Fix this test")
     def test_good_signup_with_email_error(self, client, mocker):
-        mocker.patch.object(emailer, "apply_async", return_value=None, side_effect=OperationalError)
+        mocker.patch.object(send_email, "apply_async", return_value=None, side_effect=OperationalError)
 
         payload = {
             "username": "TEST",
